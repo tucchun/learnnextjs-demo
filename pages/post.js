47a16536218ae6ddb7e1b28d549147db21c16bee
涂传春 <tuchuanchun@tuchuanchundeMacBook-Pro.local>
2018-07-01 21:05:30 +0800

    upload

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,60 @@
-import Layout from '../components/MyLayout.js'
+import Layout from "../components/MyLayout.js";
+import fetch from "isomorphic-unfetch";
+import Markdown from "react-markdown";
 
-export default (props) => (
-    <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>This is the blog post content.</p>
-    </Layout>
-)
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <div className="markdown">
+      <Markdown
+        source={`
+        This is our blog post.
+        Yes. We can have a [link](/link).
+        And we can have a titile as well.
+
+        ### This is a title
+
+
+        And here's the content.
+      `}
+      />
+    </div>
+    {/* <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p> */}
+    {/* <img src={props.show.image.medium} /> */}
+    <style jsx global>
+      {`
+        .markdown {
+          font-family: "Arial";
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}
+    </style>
+  </Layout>
+);
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+  return { show };
+};
+
+export default Post;
